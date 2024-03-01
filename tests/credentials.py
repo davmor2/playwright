6e9d9ad0b6cb8a9f0249589261c5ccac9d0685f4
davmor2 <davmor2@davmor2.co.uk>
2024-03-01 21:25:23 +0000

    Update credentials.py

--- tests/credentials.py
+++ tests/credentials.py
@@ -4,3 +4,5 @@ class Creds:
   def __init__(self):
     self.custusername = os.environ['custname']
     self.custpassword = os.environ['custpassword']
+    self.resellerusername = os.environ['resellername']
+    self.resellerpassword = os.environ['resellerpassword']

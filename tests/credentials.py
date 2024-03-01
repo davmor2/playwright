3ec5d295730516cc78b7d1542667da89de4e9660
davmor2 <davmor2@davmor2.co.uk>
2024-03-01 21:57:00 +0000

    Update credentials.py

--- tests/credentials.py
+++ tests/credentials.py
@@ -2,7 +2,7 @@ import os
 
 class Creds:
     def __init__(self):
-        self.custusername = os.getenv('custname')
-        self.custpassword = os.getenv('custpassword')
-        self.resellerusername = os.getenv('resellername')
-        self.resellerpassword = os.getenv('resellerpassword')
+        self.custusername = os.environ.get('custname')
+        self.custpassword = os.environ.get('custpassword')
+        self.resellerusername = os.environ.get('resellername')
+        self.resellerpassword = os.environ.get('resellerpassword')
